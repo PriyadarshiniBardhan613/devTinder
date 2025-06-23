691aa9f3c381f33bff5d151f6c578a14cad96a33
unknown <bardhanpriyadarshini90@gmail.com>
2025-06-23 13:38:57 +0530

    Tested API CALLS

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
-app.use("/hello", (req, res) => {
+//app.use("/", (req, res) => {
+    //console.log('Server listening....');
+    //res.send("Basic");
+//})
+//app.use("/hello", (req, res) => {
+   // console.log('Server listening....');
+    //res.send("Hello from server...");
+//})
+app.delete("/test", (req, res) => {
     console.log('Server listening....');
-    res.send("Hello from server...");
+    res.send("Data Deleted...");
 })
-app.use("/test", (req, res) => {
+app.get("/test", (req, res) => {
     console.log('Server listening....');
     res.send("Test from server...");
+})
+app.post("/test", (req, res) => {
+    console.log('Databse recorded');
+    res.send("Data Commited to database");
 })
 app.listen(3000, () => {
     console.log("Server is listening on port 3000....");
